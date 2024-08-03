ad69626449211d71a60a8c59023d39f3e78e6257
ttfcl <ktx1016@naver.com>
2024-08-03 13:48:10 +0900

    run123

--- page/card.js
+++ page/card.js
@@ -1,7 +1,7 @@
 localStorage.getItem("userDisplayName2")
 
 
-if(localStorage.getItem("userDisplayName2") === null) {
-    alert("프로필 및 서비스 확인을 원하시면 아이디 발급을 해주세요.")
-    location.href = "../login.html"
-}
+// if(localStorage.getItem("userDisplayName2") === null) {
+//     alert("프로필 및 서비스 확인을 원하시면 아이디 발급을 해주세요.")
+//     location.href = "../login.html"
+// }
